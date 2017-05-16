b48452773d5c4183dffff3cc57d081fcb33964f3
WENDY MARTELL <wendymartelll@gmail.com>
2017-05-16 11:27:32 -0500

    added grunt imagemin plugin for image optimization with funtion included in the Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,6 +7,8 @@
 */
 'use strict'
 
+var mozjpeg = require('imagemin-mozjpeg');
+
 module.exports = function(grunt) {
 
   grunt.initConfig({
@@ -89,12 +91,38 @@ module.exports = function(grunt) {
         }]
       },
     },
+
+
+  imagemin: {                          // Task
+    static: {                          // Target
+      options: {                       // Target options
+        optimizationLevel: 3,
+        svgoPlugins: [{ removeViewBox: false }],
+        use: [mozjpeg()]
+      },
+      files: {                         // Dictionary of files
+        'dist/img.png': 'src/img.png', // 'destination': 'source'
+        'dist/img.jpg': 'src/img.jpg',
+        'dist/img.gif': 'src/img.gif'
+      }
+    },
+    dynamic: {                         // Another target
+      files: [{
+        expand: true,                  // Enable dynamic expansion
+        cwd: 'src/',                   // Src matches are relative to this path
+        src: ['**/*.{png,jpg,gif}'],   // Actual patterns to match
+        dest: 'dist/'                  // Destination path prefix
+      }]
+    }
+  },
+
   });
 
    grunt.loadNpmTasks('grunt-responsive-images');
+   grunt.loadNpmTasks('grunt-contrib-imagemin');
    grunt.loadNpmTasks('grunt-contrib-clean'); //COMMENT --> this is out because we can only run 1 file at the time to get 1x pics. also because we dont want to
    grunt.loadNpmTasks('grunt-contrib-copy');  //            delete, make dir, copy AGAIN that is why the tasks are out.
    grunt.loadNpmTasks('grunt-mkdir');
-   grunt.registerTask('default', ['clean', 'mkdir', 'copy', 'responsive_images:dev1', 'responsive_images:dev2']);
+   grunt.registerTask('default', ['clean', 'mkdir', 'copy', 'responsive_images:dev1', 'responsive_images:dev2', 'imagemin']);
   //grunt.registerTask('default', ['responsive_images:dev1', 'responsive_images:dev2']); // use it whe you want to add pictures to the dir
 };
