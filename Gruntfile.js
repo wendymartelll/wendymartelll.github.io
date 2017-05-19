029836d9836fe7bbdaa059fa27c98736d6599919
WENDY MARTELL <wendymartelll@gmail.com>
2017-05-19 12:26:42 -0700

    changed from cwd: 'images_src/' to cwd: 'images/' on imagemin

--- Gruntfile.js
+++ Gruntfile.js
@@ -113,7 +113,7 @@ var mozjpeg = require('imagemin-mozjpeg');
       },
       files: [{
         expand: true,                  // Enable dynamic expansion
-        cwd: 'images_src/',                   // Src matches are relative to this path
+        cwd: 'images/',                   // Src matches are relative to this path
         src: ['**/*.{png,jpg,gif}'],   // Actual patterns to match
         dest: 'images/'                  // Destination path prefix
       }]
