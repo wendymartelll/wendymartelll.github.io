55a22e1b85894bd659527457c82580b1852d2de3
WENDY MARTELL <wendymartelll@gmail.com>
2017-05-22 13:18:09 -0700

    using only 800

--- Gruntfile.js
+++ Gruntfile.js
@@ -134,6 +134,6 @@ var mozjpeg = require('imagemin-mozjpeg');
    grunt.loadNpmTasks('grunt-contrib-copy');  //            delete, make dir, copy AGAIN that is why the tasks are out. 'imagemin:dynamic', 'imagemin:dynamic',
    grunt.loadNpmTasks('grunt-mkdir');
    grunt.registerTask('default', ['clean', 'mkdir', 'copy', 'responsive_images:dev1',
-        'responsive_images:dev2','imagemagick-convert:dev']);
+        'responsive_images:dev2']);
   //grunt.registerTask('default', ['responsive_images:dev1', 'responsive_images:dev2']); // use it whe you want to add pictures to the dir
 };
